1c6a7e3e8992498984244367e237f0bd42c37830
Alejandro <alejogs4@gmail.com>
2018-02-28 13:29:10 -0500

    Agrega proyecto de la API

---
+++ API/src/api/API.java
@@ -0,0 +1,22 @@
+package api;
+import java.util.*;
+import java.util.function.Consumer;
+/**
+ * @author Alejandro Garcia Serna
+ */
+public class API {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Persons p1 = new Persons(1L,"Puskas");
+        Persons p2 = new Persons(2L,"Barbosa");
+        Persons p3 = new Persons(3L,"Miguel");
+        ArrayList<Persons> personsList = new ArrayList<>();
+        
+        personsList.add(p1);
+        personsList.add(p2);
+        personsList.add(p2);
+    }
+    
+}
